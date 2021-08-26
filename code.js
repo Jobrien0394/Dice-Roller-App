760b625c73d8ee8133f6562cd5d95002e1fa9b92
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-08-26 18:32:46 +0000

    deploy: 5a6259829cc1be9c0b5eb7fbb3683e7f1abe6325

--- code.js
+++ code.js
@@ -1,4 +1,80 @@
 let results = [null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-// Your Code Here!
+function rollDie(){
+  return Math.round(Math.random()*5 + 1);
+}
 
+
+function rollDice(){ 
+  return rollDie() + rollDie();
+}
+
+rollButton      =  document.getElementById("roll");
+resultBox       =  document.getElementById("resultBox");
+graphBox        =  document.getElementById("graph");
+
+thouButton = document.getElementById("rollathou"); 
+
+onRollButtonClick=function(e){
+    x=rollDice();
+    result.textContent="You rolled a "+x+"!";
+    results[x]++;
+    console.log(e+ " <sofar>: " + results[x]);
+    console.log(" " + e+ " <rolled>: " + x);
+    renderGraph();
+}
+rollButton.addEventListener('click',onRollButtonClick);
+
+
+onAThouClick=function(e){
+  for(i=0;i<1000;i++){
+    x=rollDice();
+    results[x]++;
+  }
+  result.textContent="You rolled a thousand times!";
+  renderGraph();
+}
+thouButton.addEventListener('click',onAThouClick);
+
+
+function blueBoxHeight(k){
+  FACTOR=150.0;
+  if(k<2 || k>12)
+    return "0px";
+  n_k=results[k];
+  n_nu=results.reduce( (S,n) => {return (n===null?S:Math.max(S,n))} , 1);
+  N=results.reduce(   (S,n) => {return S+(n===null?0:n)} , 0);;
+  P_k=n_k/n_nu*FACTOR;
+  return P_k + "px";
+}
+
+
+renderGraph=function(){
+    document.querySelectorAll(" .graphbox-item").forEach(function(x){x.remove()})
+
+
+    results.forEach(function(n,i){
+
+        if (! (n===null)){
+          lambda=document.createElement("div");
+          lambda.classList="graphbox-item";
+
+          kappa=document.createElement("div");
+          kappa.classList="bluebox";
+          kappa.style.height=blueBoxHeight(i);
+
+          mu=document.createElement("div");
+          mu.classList="graphbox-item-div1";
+          mu.textContent="You Rolled a "+i+" :";
+
+          nu=document.createElement("div");
+          nu.classList="graphbox-item-div2";
+          nu.textContent=`${n} times`;
+
+          lambda.append(kappa);
+          lambda.append(mu);
+          lambda.append(nu);
+          graphBox.append(lambda);
+        }
+    });
+}
